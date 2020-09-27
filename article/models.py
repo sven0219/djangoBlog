ed6727463ae80b4c23990aeffab1712b1d6c78ff
wenqiangshen <wenqiangshen@wenqiangshendeMacBook-Pro.local>
2020-09-27 19:59:42 +0800

    init commit

---
+++ article/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+
+
+class ArticlePost(models.Model):
+    
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=100)
+
+    body = models.TextField()
+
+    created = models.DateTimeField(default=timezone.now)
+
+    updated = models.DateTimeField(auto_now=True)
