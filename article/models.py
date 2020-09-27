29e2a6d8feb01f628a44d1d0a50ad005df0a0390
wenqiangshen <wenqiangshen@wenqiangshendeMacBook-Pro.local>
2020-09-27 20:18:41 +0800

    add artuclePost model

--- article/models.py
+++ article/models.py
@@ -7,12 +7,15 @@ from django.utils import timezone
 
 class ArticlePost(models.Model):
     
+  
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-
     title = models.CharField(max_length=100)
-
     body = models.TextField()
-
     created = models.DateTimeField(default=timezone.now)
-
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+    # s
+    def __str__(self):
+        return self.title
