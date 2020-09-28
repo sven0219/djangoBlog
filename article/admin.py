a4fc2dcaf83ad82ac0b3ad08bfa23aecb04aedd3
sven <swq1227@gmail.com>
2020-09-28 11:06:40 +0800

    add views

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ArticlePost
 
 # Register your models here.
+
+admin.site.register(ArticlePost)
--- article/views.py
+++ article/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def article_list(request):
+    return HttpResponse("Hello World!")
+    
