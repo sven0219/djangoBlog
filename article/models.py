e1e672878eaeff54329b37b9599fce06fb484dde
wenqiangshen <wenqiangshen@wenqiangshendeMacBook-Pro.local>
2020-09-28 11:05:06 +0800

    add views

--- article/models.py
+++ article/models.py
@@ -2,13 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-# Create your models here.
-
-
 class ArticlePost(models.Model):
-    
-  
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     body = models.TextField()
     created = models.DateTimeField(default=timezone.now)
@@ -16,6 +11,6 @@ class ArticlePost(models.Model):
 
     class Meta:
         ordering = ('-created',)
-    # s
+
     def __str__(self):
         return self.title
--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'article'
 
 urlpatterns = [
-    
+    path('article-list/',views.article_list,name='article-list'),
 ]
