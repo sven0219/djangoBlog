da93a61ec35f3cffb933e273176ca3f24ed8e428
sven <swq1227@gmail.com>
2020-09-28 16:17:03 +0800

    update

--- article/urls.py
+++ article/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'article'
 
 urlpatterns = [
-    path('article-list/',views.article_list,name='article-list'),
+    path('article-list/',views.article_list,name='article_list'),
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ArticlePost
+import markdown
 
 
 def article_list(request):
     articles = ArticlePost.objects.all()
     context = {'articles':articles}
     return render(request,'article/list.html',context) 
-       
+
+def article_detail(request,id):
+    article = ArticlePost.objects.get(id=id)
+    article.body = markdown.markdown(article.body,
+        extensions=[
+        # 包含 缩写、表格等常用扩展
+        'markdown.extensions.extra',
+        # 语法高亮扩展
+        'markdown.extensions.codehilite',
+        ])
+    context = { 'article': article }
+    return render(request, 'article/detail.html', context)
