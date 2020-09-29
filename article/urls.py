22d00cfaaa82ec7a6d6e0eea5d39719b739ac2e2
sven <swq1227@gmail.com>
2020-09-29 13:38:23 +0800

    add form

--- article/urls.py
+++ article/urls.py
@@ -6,4 +6,5 @@ app_name = 'article'
 urlpatterns = [
     path('article-list/',views.article_list,name='article_list'),
     path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    path('article-create/', views.article_create, name='article_create'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import ArticlePost
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
 import markdown
 
 
@@ -20,3 +22,19 @@ def article_detail(request,id):
         ])
     context = { 'article': article }
     return render(request, 'article/detail.html', context)
+
+
+def article_create(request):
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    else:
+        article_post_form = ArticlePostForm()
+        context = { 'article_post_form': article_post_form }
+        return render(request, 'article/create.html', context)
