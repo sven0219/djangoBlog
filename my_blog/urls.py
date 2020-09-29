4e7ea6f845cd7cf5232fa64cfbf67f0a88f25299
sven <swq1227@gmail.com>
2020-09-29 14:51:19 +0800

    add login and logout

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('article/', include('article.urls', namespace='article')),
+    path('userprofile/', include('userprofile.urls', namespace='userprofile')),
 ]
---
+++ userprofile/url.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'userprofile'
+
+urlpatterns = [
+    # 用户登录
+    path('login/', views.user_login, name='login'),
+    # 用户登出
+    path('logout/',views.user_logout,name='logout')
+]
