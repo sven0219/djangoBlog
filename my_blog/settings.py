f63671a2e0bbe6d9d27da23621166015db7169a2
sven <swq1227@gmail.com>
2020-10-09 14:22:48 +0800

    add reset password

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'article',
     'userprofile',
+    'password_reset',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,16 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 STATIC_URL = '/static/'
+
+# SMTP服务器，改为你的邮箱的smtp!
+EMAIL_HOST = 'smtp.qq.com'
+# 改为你自己的邮箱名！
+EMAIL_HOST_USER = 'your_email_account@xxx.com'
+# 你的邮箱密码
+EMAIL_HOST_PASSWORD = 'your_password'
+# 发送邮件的端口
+EMAIL_PORT = 25
+# 是否使用 TLS
+EMAIL_USE_TLS = True
+# 默认的发件人
+DEFAULT_FROM_EMAIL = 'xxx的博客 <your_email_account@xxx.com>'
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('article/', include('article.urls', namespace='article')),
     path('userprofile/', include('userprofile.urls', namespace='userprofile')),
+    path('password-reset/',include('password_reset.urls')),
 ]
